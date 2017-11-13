38c6863fbc22c72ba38af32aa7c646135a7aeb36
Sighmir <guistoppa1995@hotmail.com>
2017-11-13 14:02:06 -0200

    Added proxy to server
    Added proxy to server

--- vrp_basic_menu/server.lua
+++ vrp_basic_menu/server.lua
@@ -8,6 +8,7 @@ vRPclient = Tunnel.getInterface("vRP")
 BMclient = Tunnel.getInterface("vrp_basic_menu")
 BSclient = Tunnel.getInterface("vrp_barbershop")
 Tunnel.bindInterface("vrp_basic_menu",vRPbm)
+Proxy.addInterface("vrp_basic_menu",vRPbm)
 lcfg = module("vrp", "cfg/base")
 -- load global and local languages
 Luang = module("vrp", "lib/Luang")
